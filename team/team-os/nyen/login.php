6ab8d8ab19ecd2aa3fbf5227af8c39d1742fc641
Nini Yen <yensydney@gmail.com>
2024-06-29 17:15:07 -0700

    trying login

---
+++ team/team-os/nyen/login.php
@@ -0,0 +1,46 @@
+<?php
+    include('database.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>HopeKCC Login</title>
+</head>
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+        <h2>Login</h2>
+        Username:<br>
+        <input type="text" name="username" required><br>
+        Password:<br>
+        <input type="Password" name="password" required><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $sql = "SELECT Password FROM Student where Username like '$username'";
+        $result = mysqli_query($conn, $sql);
+        $resultarray = mysqli_fetch_all($result);
+
+        echo $resultarray;
+
+        // if () {
+        //     echo "That username is taken. Try again.";
+        // }
+
+        // else {
+        //     $sql = "INSERT INTO Student (FirstName, LastName, Username, Password)
+        //             VALUES ('$firstname', '$lastname', '$username', '$password')";
+        //     mysqli_query($conn, $sql);
+        //     echo"You are now registered!";
+        // }
+        
+    }
+
+    mysqli_close($conn);
+?>
+</body>
+</html>
--- team/team-os/nyen/v2index.php
+++ team/team-os/nyen/v2index.php
@@ -20,25 +20,12 @@
         <input type="Password" name="password" required><br>
         <input type="submit" name="submit" value="Submit">
     </form>
-    <!-- <form action="" method="post">
-        <h2>Login</h2>
-        Username:<br>
-        <input type="text" name="username1" required><br>
-        Password:<br>
-        <input type="Password" name="password1" required><br>
-        <input type="submit" name="submit" value="Submit">
-    </form> -->
 <?php
-    // echo 'hi';
-
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $firstname = filter_input(INPUT_POST, "firstname", FILTER_SANITIZE_SPECIAL_CHARS);
         $lastname = filter_input(INPUT_POST, "lastname", FILTER_SANITIZE_SPECIAL_CHARS);
         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
         $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-
-        // echo 'Hi, ' . $firstname . ', ' . $lastname . ', ' . $username . '<br>';
-
 
         // $hash = password_hash($password, PASSWORD_DEFAULT);
         // mysqli_query(mysqli $mysql, string $query, int $result_mode = MYSQLI_STORE_RESULT): mysqli_result|bool
@@ -47,13 +34,6 @@
         $sql = "SELECT * FROM Student where Username like '$username'";
         $result = mysqli_query($conn, $sql);
         $resultarray = mysqli_fetch_all($result);
-
-        // echo 'printing result' . '<br>';
-        // foreach($resultarray as $entries) {
-        //     foreach($entries as $field) {
-        //         echo $field;
-        //     }
-        // }
 
         if (sizeof($resultarray) > 0) {
             echo "That username is taken. Try again.";
@@ -65,19 +45,6 @@
             mysqli_query($conn, $sql);
             echo"You are now registered!";
         }
-        // }
-
-        // $sql = "SELECT * FROM Student";
-        // $result = mysqli_query($conn, $sql);
-        // $finalarray = mysqli_fetch_all($result);
-        // foreach($finalarray as $array) {
-        //     foreach($array as $item) {
-        //         echo $item . ' ';
-        //     }
-        //     echo "<br>";
-        // }
-        // echo 'done';
-
         
     }
 
