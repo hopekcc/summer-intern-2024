35d217281c71eb7422e827efa96234995ec54d7c
Nini Yen <yensydney@gmail.com>
2024-06-30 22:06:31 -0400

    adding start page

---
+++ team/team-os/nyen/Start.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<button onclick="document.location='http://test1.hopekcc.org/classroom/ext_yensydney_gmail_com/summer-intern-2024/team/team-os/nyen/v2index.php'">Sign-up</button>
+<button onclick="document.location='http://test1.hopekcc.org/classroom/ext_yensydney_gmail_com/summer-intern-2024/team/team-os/nyen/login.php'">Login</button>
+
+</body>
+</html>
