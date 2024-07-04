5a51eaf314259e5b8e8c06e36221547f9b85949c
Nini Yen <yensydney@gmail.com>
2024-07-03 22:39:16 -0400

    testing separate files

--- team/team-os/nyen/login.php
+++ team/team-os/nyen/login.php
@@ -1,22 +1,6 @@
 <?php
     include('database.php');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>HopeKCC Login</title>
-</head>
-<body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
-        <h2>Login</h2>
-        Username:<br>
-        <input type="text" name="username" required><br>
-        Password:<br>
-        <input type="Password" name="password" required><br>
-        <input type="submit" name="submit" value="Submit"><br>
-    </form>
-<?php
+    
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
         $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
