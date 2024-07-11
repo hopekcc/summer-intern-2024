00ddc371c765e6cbad526139599aab0543558ef7
yenchenlan <yenchenlan@gmail.com>
2024-07-11 12:19:54 -0700

    Created folders for all students from week 1-4

---
+++ team/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+../tools/createFolders.sh aa nathanael
+../tools/createFolders.sh aa serena
+../tools/createFolders.sh aa tina
+../tools/createFolders.sh ak jiawei
+../tools/createFolders.sh ak nathan
+../tools/createFolders.sh ak phoebe
+../tools/createFolders.sh ak van
+../tools/createFolders.sh ak yijia
+../tools/createFolders.sh ak zachary
+../tools/createFolders.sh la jonathan
+../tools/createFolders.sh la joshua
+../tools/createFolders.sh la lucas
+../tools/createFolders.sh la yuwei
+../tools/createFolders.sh os achan
+../tools/createFolders.sh os cgonzalez
+../tools/createFolders.sh os jliao
+../tools/createFolders.sh os kluo
+../tools/createFolders.sh os nyen
+../tools/createFolders.sh os vniu
+
+
+
+
