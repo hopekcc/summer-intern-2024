98fcf659a3d91d1470268d4f4b9064fde0929b80
yenchenlan <yenchenlan@gmail.com>
2024-07-11 12:28:21 -0700

    Added project folders

--- tools/createFolders.sh
+++ tools/createFolders.sh
@@ -4,10 +4,20 @@ teamID=$1
 name=$2
 mypath=${0%/*}
 
-[ -z ${name} -o -z ${teamID} ] && echo "no name or team" && 
+[ -z ${teamID} ] && echo "no team assigned" && 
     echo "usage: $0 <teamID> <name>. For example: $0 os nini" && exit
 
 team="team-${teamID}"
+
+echo "creating folders for ${team}"
+mkdir -p ${team}/project/code/
+mkdir -p ${team}/project/presentation/
+cp ${mypath}/gitignore ${team}/project/code/.gitignore
+cp ${mypath}/gitignore ${team}/project/presentation/.gitignore
+
+[ -z ${name} ] && echo "no student assigned. Skip student folders" && exit
+
+exit
 
 echo "creating folders for ${name} in ${team}"
 for i in $(seq 4); do
